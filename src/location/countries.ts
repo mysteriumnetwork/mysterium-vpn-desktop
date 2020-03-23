9445d0f853e92c5cb65a34e130439c945014202e
Tadas Krivickas <tadas.krivickas@gmail.com>
2020-03-23 11:03:08 +0200

    Restore missing link to originating repository for flags

--- src/location/countries.ts
+++ src/location/countries.ts
@@ -4,6 +4,8 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
+// base64 encoded 24px flags, originally from https://github.com/gosquared/flags (MIT)
+// https://github.com/AndrejsAbrickis/flag-icons/tree/master/src/flags (MIT)
 const countries: { [key: string]: CountryType } = Object.freeze({
     af: {
         name: "Afghanistan",
