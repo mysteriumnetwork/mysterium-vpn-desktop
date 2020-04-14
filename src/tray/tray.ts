bf43f2bd2ea64b5ecee639ce2c661471c3c7261d
Tadas Krivickas <tadas.krivickas@gmail.com>
2020-04-14 14:04:46 +0300

    Fix tray icon not showing

--- src/tray/tray.ts
+++ src/tray/tray.ts
@@ -4,16 +4,17 @@
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
  */
-import path from "path"
-
 import { QIcon, QMenu, QSystemTrayIcon } from "@nodegui/nodegui"
+
+import mystLogo from "../../assets/logo.png"
+import { fixAssetPath } from "../utils/paths"
 
 import { quitAction } from "./quit"
 import { repairAction } from "./repair"
 
 export const createSystemTray = (): void => {
     const tray = new QSystemTrayIcon()
-    tray.setIcon(new QIcon(path.resolve(__dirname, "../assets/logo.png")))
+    tray.setIcon(new QIcon(fixAssetPath(mystLogo)))
     const trayMenu = new QMenu()
     trayMenu.addAction(repairAction())
     trayMenu.addSeparator()
