1a4ae98094a430754d4bda4349c66ebb987f7e8c
Tadas Krivickas <tadas.krivickas@gmail.com>
2020-06-16 15:31:52 +0300

    Use system DNS until fixed: https://github.com/mysteriumnetwork/node/issues/2387

--- src/connection/store.ts
+++ src/connection/store.ts
@@ -116,7 +116,7 @@ export class ConnectionStore {
                     accountantId,
                     serviceType: this.root.proposals.active.serviceType,
                     connectOptions: {
-                        dns: "auto",
+                        dns: "system",
                     },
                 },
                 30000,
