c7ceed22276122e78b8e2b251da03fe6c6f1acd0
tomasmik <tomasmik@protonmail.com>
2021-02-01 10:43:32 +0200

    Show window is opened again while in tray
    If you closed the window instead of minimizing it
    it would go to the system tray, opening the app again would not bring it to focus.
    This should now be fixed.

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -171,7 +171,7 @@ if (!appInstanceLock) {
         // Someone tried to run a second instance, we should focus our window.
         if (win) {
             if (win.isMinimized()) win.restore()
-            win.focus()
+            win.show()
         }
     })
 
