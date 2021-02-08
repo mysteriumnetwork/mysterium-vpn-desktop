476c849079812bac5d281ae1185609048dde2c7c
Tadas Krivickas <tadas.krivickas@gmail.com>
2021-02-08 21:08:02 +0200

    Show warning about test tokens not to be mistaken with MYST

--- src/views/consumer/ReceiveTokens/ReceiveTokens.tsx
+++ src/views/consumer/ReceiveTokens/ReceiveTokens.tsx
@@ -7,6 +7,8 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
 import styled from "styled-components"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faExclamationTriangle } from "@fortawesome/free-solid-svg-icons"
 
 import { useStores } from "../../../store"
 import { QR } from "../../../ui-kit/components/QR/QR"
@@ -18,11 +20,20 @@ const Container = styled.div`
     padding: 12px;
 `
 
+const Disclaimer = styled.div`
+    color: #d63031;
+    font-weight: 400;
+`
+
 export const ReceiveTokens: React.FC = observer(() => {
     const { identity } = useStores()
     return (
         <Container>
             <p>Topup your wallet by sending MYSTT to the address below.</p>
+            <Disclaimer>
+                <FontAwesomeIcon icon={faExclamationTriangle} /> NOTE: MYSTT is a test token of Ethereum Görli testnet.
+                Don&apos;t send real MYST tokens to this address, they may be lost forever!
+            </Disclaimer>
             <QR text={identity.identity?.channelAddress} />
         </Container>
     )
