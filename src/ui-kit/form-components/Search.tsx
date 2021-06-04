122d14d00557352a71c8425d3bc3e4125a925527
Tadas Krivickas <tadas.krivickas@gmail.com>
2021-06-04 14:17:25 +0300

    Add node search bar

--- src/ui-kit/form-components/Search.tsx
+++ src/ui-kit/form-components/Search.tsx
@@ -5,15 +5,47 @@
  * LICENSE file in the root directory of this source tree.
  */
 import React from "react"
-
-import { TextInput } from "./TextInput"
+import styled from "styled-components"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faSearch } from "@fortawesome/free-solid-svg-icons"
 
 export interface SearchProps {
     onChange: (text?: string) => void
 }
 
+const Icon = styled(FontAwesomeIcon)`
+    position: relative;
+    z-index: 1;
+    left: 24px;
+    top: -24px;
+    color: #fff;
+    opacity: 0.2;
+`
+
+const SearchInput = styled.input`
+    background: #703a74;
+    height: 35px;
+    width: 100%;
+    padding: 10px 45px;
+    box-sizing: border-box;
+    border: 0;
+
+    border-radius: 10px;
+    box-shadow: 0px 0px 30px rgba(11, 0, 75, 0.2);
+
+    color: #fff;
+    ::placeholder {
+        color: #fff;
+    }
+`
+
 export const Search: React.FC<SearchProps> = ({ onChange, ...rest }) => {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const handleChange = (evt: any): void => onChange(evt.target.value)
-    return <TextInput placeholder="Search..." onChange={handleChange} {...rest} />
+    return (
+        <>
+            <SearchInput placeholder="Search for node..." onChange={handleChange} {...rest} />
+            <Icon icon={faSearch} />
+        </>
+    )
 }
--- src/views/consumer/SelectProposal/SelectProposalView.tsx
+++ src/views/consumer/SelectProposal/SelectProposalView.tsx
@@ -7,11 +7,14 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
 import styled from "styled-components"
+import _ from "lodash"
 
 import { CountryFilter } from "../../../proposals/components/CountryFilter/CountryFilter"
 import { ProposalTable } from "../../../proposals/components/ProposalTable/ProposalTable"
 import { SelectedProposal } from "../../../proposals/components/SelectedProposal/SelectedProposal"
 import { LogoTitle } from "../../../ui-kit/components/LogoTitle/LogoTitle"
+import { useStores } from "../../../store"
+import { Search } from "../../../ui-kit/form-components/Search"
 
 const Container = styled.div`
     flex: 1;
@@ -33,6 +36,7 @@ const Top = styled.div`
     padding-bottom: 14px;
     display: flex;
     align-items: center;
+    justify-content: space-between;
 `
 
 const Sidebar = styled.div`
@@ -71,11 +75,23 @@ const MainBottom = styled.div`
     margin-top: auto;
 `
 
+const SearchNodeWrap = styled.div`
+    width: 378px;
+    height: 35px;
+`
+
 export const SelectProposalView: React.FC = observer(() => {
+    const { proposals } = useStores()
+    const searchDebounced = _.debounce((text): void => {
+        proposals.setTextFilter(text)
+    }, 500)
     return (
         <Container>
             <Top>
                 <LogoTitle />
+                <SearchNodeWrap>
+                    <Search onChange={searchDebounced} />
+                </SearchNodeWrap>
             </Top>
             <Split>
                 <Sidebar>
