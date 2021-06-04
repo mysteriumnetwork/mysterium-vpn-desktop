2dea0228638213310904c9665ff8410eb304ef74
Tadas Krivickas <tadas.krivickas@gmail.com>
2021-06-04 09:31:39 +0300

    Switch to react-circle-flags

--- src/location/components/Flag/Flag.tsx
+++ src/location/components/Flag/Flag.tsx
@@ -5,52 +5,15 @@
  * LICENSE file in the root directory of this source tree.
  */
 import React from "react"
-
-import { isUnknownCountry } from "../../countries"
-import { log } from "../../../log/log"
+import { CircleFlag } from "react-circle-flags"
 
 export interface FlagProps {
-    countryCode?: string
     className?: string
+    countryCode?: string
 }
 
-const size = 24
-const unknownCountry = "_unknown"
-
-const resolveFlagUrls = (imageName: string): { x1: string; x2: string } => {
-    // Flags sourced from https://github.com/wiredmax/react-flags (MIT)
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const x1 = require(`./flags-iso/shiny/${size}/${imageName}.png`).default
-    // eslint-disable-next-line @typescript-eslint/no-var-requires
-    const x2 = require(`./flags-iso/shiny/${size * 2}/${imageName}.png`).default
-
-    return { x1, x2 }
-}
+const size = 15
 
 export const Flag: React.FC<FlagProps> = ({ className, countryCode = "unknown" }) => {
-    const imageName = isUnknownCountry(countryCode) ? unknownCountry : countryCode
-    try {
-        const flags = resolveFlagUrls(imageName)
-        return (
-            <img
-                className={className}
-                srcSet={`${flags.x1} 1x, ${flags.x2} 2x`}
-                src={flags.x1}
-                alt={countryCode}
-                width={size}
-            />
-        )
-    } catch (e) {
-        log.error(`could not resolve flag icon for countryCode: ${countryCode}`, e)
-        const flags = resolveFlagUrls(unknownCountry)
-        return (
-            <img
-                className={className}
-                srcSet={`${flags.x1} 1x, ${flags.x2} 2x`}
-                src={flags.x1}
-                alt={countryCode}
-                width={size}
-            />
-        )
-    }
+    return <CircleFlag className={className} countryCode={countryCode?.toLowerCase() ?? "unknown"} height={size} />
 }
