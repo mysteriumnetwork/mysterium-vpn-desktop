c0ac49ebb76c2fc0ceb27e9bc8591cc3b55e60e2
Tadas Krivickas <tadas.krivickas@gmail.com>
2021-06-22 12:36:46 +0300

    Show terms in the terms page

--- src/views/common/Help/HelpContentTermsAndConditions.tsx
+++ src/views/common/Help/HelpContentTermsAndConditions.tsx
@@ -7,17 +7,53 @@
 import React from "react"
 import styled from "styled-components"
 import { observer } from "mobx-react-lite"
+import { TermsEndUser } from "@mysteriumnetwork/terms"
+import ReactMarkdown from "react-markdown"
+import * as termsPackageJson from "@mysteriumnetwork/terms/package.json"
 
-import { Heading2 } from "../../../ui-kit/typography"
+import { Heading2, Small } from "../../../ui-kit/typography"
+import { brand } from "../../../ui-kit/colors"
 
 const Title = styled(Heading2)`
-    margin: 15px 0;
+    margin-bottom: 6px;
+`
+
+const TermsMeta = styled.div`
+    color: ${brand};
+    margin-bottom: 10px;
+`
+
+const Terms = styled.div`
+    width: 325px;
+    // height: 288px;
+    box-sizing: border-box;
+    margin: 0 auto;
+    padding: 0 6px;
+    word-wrap: break-word;
+    overflow-y: scroll;
+    font-size: 13px;
+    color: #fff;
+    opacity: 0.7;
+    user-select: text;
+    -webkit-app-region: no-drag;
+
+    ul {
+        padding-inline-start: 20px;
+    }
 `
 
 export const HelpContentTermsAndConditions: React.FC = observer(() => {
     return (
         <>
             <Title>Terms & Conditions</Title>
+            <TermsMeta>
+                Version: {termsPackageJson.version} / Last updated: {termsPackageJson.updatedAt ?? ""}
+            </TermsMeta>
+            <Terms>
+                <Small>
+                    <ReactMarkdown source={TermsEndUser} />
+                </Small>
+            </Terms>
         </>
     )
 })
