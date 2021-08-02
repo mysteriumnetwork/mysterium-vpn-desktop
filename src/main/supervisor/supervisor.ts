19c46ca7287cdeee3eadee5c946a3e290d9c8c63
Tadas Krivickas <tadas.krivickas@gmail.com>
2021-08-02 13:25:12 +0300

    Improve CLI error message parsing

--- src/main/supervisor/supervisor.ts
+++ src/main/supervisor/supervisor.ts
@@ -235,29 +235,35 @@ export class Supervisor implements SupervisorInterface {
         passphrase: string
     }): Promise<IpcResponse> {
         return new Promise((resolve) => {
-            const cli = spawn(
-                this.mystBin(),
-                [
-                    "cli",
-                    "--agreed-terms-and-conditions",
-                    `--tequilapi.port=${TEQUILAPI_PORT}`,
-                    "identities",
-                    "export",
-                    id,
-                    passphrase,
-                    filename,
-                ],
-                { stdio: "inherit" },
-            )
+            const cli = spawn(this.mystBin(), [
+                "cli",
+                "--agreed-terms-and-conditions",
+                `--tequilapi.port=${TEQUILAPI_PORT}`,
+                "identities",
+                "export",
+                id,
+                passphrase,
+                filename,
+            ])
+            let err = ""
+            cli.stdout?.on("data", (data) => {
+                const message = data.toString()
+                const idx = message.indexOf("Possible error: ")
+                if (idx != -1) {
+                    err = message.substr(idx)
+                }
+            })
             cli.on("exit", (code) => {
                 if (code == 0) {
                     return resolve({
                         result: filename,
                     })
                 } else {
-                    return resolve({
-                        error: "Failed with status: " + code,
-                    })
+                    if (err) {
+                        return resolve({ error: err })
+                    } else {
+                        return resolve({ error: "Failed with status: " + code })
+                    }
                 }
             })
         })
@@ -265,28 +271,34 @@ export class Supervisor implements SupervisorInterface {
 
     importIdentity({ filename, passphrase }: ImportIdentityOpts): Promise<IpcResponse> {
         return new Promise((resolve) => {
-            const cli = spawn(
-                this.mystBin(),
-                [
-                    "cli",
-                    "--agreed-terms-and-conditions",
-                    `--tequilapi.port=${TEQUILAPI_PORT}`,
-                    "identities",
-                    "import",
-                    passphrase,
-                    filename,
-                ],
-                { stdio: "inherit" },
-            )
+            const cli = spawn(this.mystBin(), [
+                "cli",
+                "--agreed-terms-and-conditions",
+                `--tequilapi.port=${TEQUILAPI_PORT}`,
+                "identities",
+                "import",
+                passphrase,
+                filename,
+            ])
+            let err = ""
+            cli.stdout?.on("data", (data) => {
+                const message = data.toString()
+                const idx = message.indexOf("Possible error: ")
+                if (idx != -1) {
+                    err = message.substr(idx)
+                }
+            })
             cli.on("exit", (code) => {
                 if (code == 0) {
                     return resolve({
                         result: filename,
                     })
                 } else {
-                    return resolve({
-                        error: "Failed with status: " + code,
-                    })
+                    if (err) {
+                        return resolve({ error: err })
+                    } else {
+                        return resolve({ error: "Failed with status: " + code })
+                    }
                 }
             })
         })
