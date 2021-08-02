dfbb360d284f56d357005ad773d038d9fe70cb8e
Tadas Krivickas <tadas.krivickas@gmail.com>
2021-08-02 13:50:15 +0300

    Add Mysterium ID backup step to onboarding

---
+++ src/app/onboarding/components/IdentityBackup/IdentityBackup.tsx
@@ -0,0 +1,141 @@
+/**
+ * Copyright (c) 2021 BlockDev AG
+ *
+ * This source code is licensed under the MIT license found in the
+ * LICENSE file in the root directory of this source tree.
+ */
+import { observer } from "mobx-react-lite"
+import React, { useState } from "react"
+import { faClock, faFileExport } from "@fortawesome/free-solid-svg-icons"
+import styled from "styled-components"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import Lottie from "react-lottie-player"
+import { useToasts } from "react-toast-notifications"
+
+import { ViewContainer } from "../../../navigation/components/ViewContainer/ViewContainer"
+import { ViewSplit } from "../../../navigation/components/ViewSplit/ViewSplit"
+import { ViewSidebar } from "../../../navigation/components/ViewSidebar/ViewSidebar"
+import { ViewContent } from "../../../navigation/components/ViewContent/ViewContent"
+import { ViewNavBar } from "../../../navigation/components/ViewNavBar/ViewNavBar"
+import { Heading2, Small } from "../../../ui-kit/typography"
+import {
+    ButtonContent,
+    ButtonIcon,
+    PrimarySidebarActionButton,
+    SecondarySidebarActionButton,
+} from "../../../ui-kit/components/Button/SidebarButtons"
+import { useStores } from "../../../store"
+import { ExportIdentityPrompt } from "../../../views/common/Settings/ExportIdentityPrompt"
+
+import animationIdentityKeys from "./animation_identity_keys.json"
+
+const SideTop = styled.div`
+    box-sizing: border-box;
+    height: 136px;
+    padding: 20px;
+    overflow: hidden;
+    text-align: center;
+`
+
+const SectionIcon = styled(FontAwesomeIcon)`
+    margin-bottom: 15px;
+    font-size: 20px;
+`
+
+const Title = styled(Heading2)`
+    margin-bottom: 15px;
+`
+
+const SideBot = styled.div`
+    background: #fff;
+    box-shadow: 0px 0px 30px rgba(11, 0, 75, 0.1);
+    border-radius: 10px;
+    box-sizing: border-box;
+    padding: 20px;
+    flex: 1 0 auto;
+
+    display: flex;
+    flex-direction: column;
+`
+
+const Content = styled(ViewContent)`
+    background: none;
+    justify-content: center;
+`
+
+const ToastWrap = styled.span`
+    word-wrap: break-word;
+    overflow-wrap: anywhere;
+`
+
+export const IdentityBackup: React.FC = observer(() => {
+    const { onboarding, identity } = useStores()
+    const { addToast } = useToasts()
+
+    const nextStep = () => {
+        onboarding.finishIdSetup()
+    }
+    const handleBackupLater = nextStep
+
+    const [exportPrompt, setExportPrompt] = useState(false)
+    const handleBackupNow = () => {
+        setExportPrompt(true)
+    }
+    const handleExportSubmit = async ({ passphrase }: { passphrase: string }) => {
+        setExportPrompt(false)
+        const res = await identity.exportIdentity({ id: identity.identity?.id ?? "", passphrase })
+        if (res.result) {
+            nextStep()
+        } else if (res.error) {
+            addToast(<ToastWrap>Identity backup failed. Error: {res.error}</ToastWrap>, {
+                appearance: "error",
+                autoDismiss: true,
+            })
+        }
+    }
+    const handleExportCancel = () => {
+        setExportPrompt(false)
+    }
+    return (
+        <ViewContainer>
+            <ViewNavBar />
+            <ViewSplit>
+                <ViewSidebar>
+                    <SideTop>
+                        <SectionIcon icon={faFileExport} />
+                        <Title>Create backup</Title>
+                        <Small>We don&apos;t store any account data. Back up to keep your tokens safe.</Small>
+                    </SideTop>
+                    <SideBot>
+                        <PrimarySidebarActionButton onClick={handleBackupNow}>
+                            <ButtonContent>
+                                <ButtonIcon>
+                                    <FontAwesomeIcon icon={faFileExport} />
+                                </ButtonIcon>
+                                Backup Private Key
+                            </ButtonContent>
+                        </PrimarySidebarActionButton>
+                        <SecondarySidebarActionButton onClick={handleBackupLater}>
+                            <ButtonContent>
+                                <ButtonIcon>
+                                    <FontAwesomeIcon icon={faClock} />
+                                </ButtonIcon>
+                                Backup later
+                            </ButtonContent>
+                        </SecondarySidebarActionButton>
+                    </SideBot>
+                </ViewSidebar>
+                <Content>
+                    <Lottie
+                        play
+                        loop={false}
+                        animationData={animationIdentityKeys}
+                        style={{ width: 256, height: 256 }}
+                        renderer="svg"
+                    />
+                </Content>
+            </ViewSplit>
+            <ExportIdentityPrompt visible={exportPrompt} onSubmit={handleExportSubmit} onCancel={handleExportCancel} />
+        </ViewContainer>
+    )
+})
--- src/app/views/common/Onboarding/OnboardingView.tsx
+++ src/app/views/common/Onboarding/OnboardingView.tsx
@@ -27,68 +27,8 @@ export const OnboardingView: React.FC = observer(() => {
                 <Route exact path={locations.onboardingIdentitySetup.path}>
                     <IdentitySetup />
                 </Route>
-                <Route exact path={locations.onboarding3.path}>
-                    <Container>
-                        <Steps>
-                            <StepProgressBar step={2} />
-                        </Steps>
-                        <Title>Surf the web, and pay as you go</Title>
-                        <Animation>
-                            <Lottie
-                                play
-                                loop
-                                animationData={animationPayAsYouGo}
-                                style={{ width: 256, height: 256 }}
-                                renderer="svg"
-                            />
-                        </Animation>
-                        <Subtitle>No lock in subscriptions</Subtitle>
-                        <Description>
-                            Using our micropayments system, Hermes Protocol, you only pay for the gigabytes you actually
-                            use.
-                            <br />
-                            No subscriptions, no monthly fees – just minute-by-minute payments.
-                        </Description>
-                        <Actions>
-                            <BackButton onClick={() => router.history?.goBack()}>Back</BackButton>
-                            <NextButton
-                                onClick={(): void => {
-                                    router.push(locations.onboarding4)
-                                }}
-                            >
-                                Next
-                            </NextButton>
-                        </Actions>
-                        <SkipContainer>
-                            <GhostButton onClick={handleSkip}>Skip</GhostButton>
-                        </SkipContainer>
-                    </Container>
-                </Route>
-                <Route exact path={locations.onboarding4.path}>
-                    <Container>
-                        <Steps>
-                            <StepProgressBar step={3} />
-                        </Steps>
-                        <Title>Top up with popular cryptocurrencies</Title>
-                        <Animation>
-                            <Lottie
-                                play
-                                loop
-                                animationData={animationPayAsYouGo}
-                                style={{ width: 256, height: 256 }}
-                                renderer="svg"
-                            />
-                        </Animation>
-                        <Subtitle>BTC, ETH, LTC, BTH and more</Subtitle>
-                        <Description>
-                            Top up your account now or do it later and use limited functionality and free nodes
-                        </Description>
-                        <Actions>
-                            <BackButton onClick={() => router.history?.goBack()}>Back</BackButton>
-                            <NextButton onClick={handleFinish}>Setup my account</NextButton>
-                        </Actions>
-                        <SkipContainer />
-                    </Container>
+                <Route exact path={locations.onboardingIdentityBackup.path}>
+                    <IdentityBackup />
                 </Route>
                 <Redirect to={locations.onboardingWelcome.path} />
             </Switch>
