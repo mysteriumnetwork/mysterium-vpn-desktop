daf5f72d560f038684a343a7682cce75ffc13c81
otkd <7527203+otkd@users.noreply.github.com>
2022-04-08 13:46:22 -0400

    Bump copyright year
    Signed-off-by: otkd <7527203+otkd@users.noreply.github.com>

--- src/app/payment/methods.ts
+++ src/app/payment/methods.ts
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2021 BlockDev AG
+ * Copyright (c) 2022 BlockDev AG
  *
  * This source code is licensed under the MIT license found in the
  * LICENSE file in the root directory of this source tree.
